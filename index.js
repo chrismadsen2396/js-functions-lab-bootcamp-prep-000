f44a4fd971769e0b3d9f1c2b4a02d91490fa81d3
chrismadsen2396@gmail.com <chrismadsen2396@gmail.com>
2018-02-27 22:03:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ function happyHolidaysTo(name) {
 }
 
 function happyHolidayTo(holiday, name) {
-  return `Happy ${holiday}, ${name}`
+  return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountDown(holiday, days) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} day until ${holiday}!`
 }
